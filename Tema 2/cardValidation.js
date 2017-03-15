584a02bd9b548a73c3d7cd0e472e37b98e98bf55
gustavo <guske6@gmail.com>
2017-03-15 11:44:31 +0100

    cambios realizados

--- Tema 2/cardValidation.js
+++ Tema 2/cardValidation.js
@@ -1,10 +1,13 @@
 function validationCard(cardNumber){
     
     var str = cardNumber;
-    
     var numbers = str.replace(/-/g,"");
     
-    if (isNaN(numbers)){return false;}
+    var resultado = {valid:true, CardID:cardNumber, error:"sin errores"};
+    
+    if (isNaN(numbers)){resultado.valid = false; 
+                        resultado.error = "contiene algun caracter no numerico"; 
+                        return resultado;}
     
     if (numbers.length == 16){
         var acaba = false;
@@ -20,24 +23,34 @@ function validationCard(cardNumber){
                 acaba = true;
             }
             
-        } if ( acaba == false ) {return acaba;}
+        } if ( acaba == false ) {resultado.valid = false; 
+                                 resultado.error = "todos los numeros son iguales"; 
+                                 return resultado;}
         
           else {
               numfinal = numbers[numbers.length-1];
               if ( numfinal%2 != 0 ){
-                  return false;
+                  resultado.valid = false;
+                  resultado.error = "el ultimo numero no es par";
+                  return resultado;
               }
+            
+              var sumanums = 0;
               
           }for (n in numbers){
               
-              var sumanums = numbers[n]+ sumanums;
-              
-              if (sumanums < 16){
-                  return false;
-              }
-              else {return true;}
+              sumanums = parseInt(numbers[n]) + sumanums;
+             
           }
+            if (sumanums < 16){
+                resultado.valid = false;
+                resultado.error = "la suma de los numeros no es superior a 16"
+                return resultado;
+            } else {return resultado;}
         
-    } else {return false;}
+    } else {
+        resultado.valid = false;
+        resultado.error = "No contiene 16 caracteres de longitud"
+        return resultado;}
     
 }
