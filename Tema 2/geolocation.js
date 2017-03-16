95c40323cb2bf53fb45af6c1df539a6851d3878e
gustavo <guske6@gmail.com>
2017-03-16 11:15:35 +0100

    agregado maps

--- Tema 2/geolocation.js
+++ Tema 2/geolocation.js
@@ -1,6 +1,6 @@
 var long = document.getElementById("longitud");
 var lat = document.getElementById("latitud");
-var err = document.getElementByID("errors");
+var err = document.getElementById("errors");
 
 function getLocation() {
     if (navigator.geolocation) {
@@ -13,6 +13,8 @@ function getLocation() {
 function showPosition(position) {
     lat.innerHTML = position.coords.latitude;
     long.innerHTML = position.coords.longitude;
+    initMap(position.coords.latitude, position.coords.longitude);
+
 }
 
 function showError(error) {
@@ -32,9 +34,27 @@ function showError(error) {
     }
 }
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.1923129, lng: -3.6174496},
-    zoom: 6
+function initMap(lati, longi) {
+  var map = new google.maps.Map(document.getElementById('mapa'), {
+    center: {lat: lati, lng: longi},
+    zoom: 15
   });
   var infoWindow = new google.maps.InfoWindow({map: map});
+  var marker = new google.maps.Marker({map: map, position:{
+        lat: lati,
+        lng: longi},
+        title: 'Estas aqui',
+        animation: google.maps.Animation.DROP,});
+    marker.addListener('click', toggleBounce);
+    
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+    
+}
+
+
