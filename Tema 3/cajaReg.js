0244b38374ee406008062f902110e8d10c2105ae
gustavo <guske6@gmail.com>
2017-03-29 09:52:33 +0200

    agregado caja reg (faltan cosas no necesarias)

--- Tema 3/cajaReg.js
+++ Tema 3/cajaReg.js
@@ -13,19 +13,19 @@ class Empleado {
 }
 
 class Producto {
-    constructor(nombreProducto, precio){
+    constructor(idproducto, nombreProducto, precio){
+        this.idproducto = idproducto;
         this.articulo = nombreProducto;
         this.precio = precio; 
     }
     
     muestraProducto(){
         
-        return "producto: " + this.articulo + "\n precio: " + this.precio + "€";  
+        return "ID: " + this.idproducto + "\n producto: " + this.articulo + "\n precio: " + this.precio + "€";  
     }
 }
 
 class CajaRegistradora {
-    
     
     constructor(){
         this.listaEmpleados = [];
@@ -47,9 +47,9 @@ class CajaRegistradora {
         }
     }
     
-    introducirProducto(nomprod, prec){
+    introducirProducto(idprod, nomprod, prec){
         if (this.empactivo){
-            this.listaProductos.push(new Producto(nomprod, prec));
+            this.listaProductos.push(new Producto(idprod, nomprod, prec));
         }
         
         else{
@@ -64,19 +64,35 @@ class CajaRegistradora {
         }
     }
     
-    identificarEmpleado(idem){
+    identificarEmpleado(ident){
         for (var i=0; i<this.listaEmpleados.length; i++){
-            if (this.listaEmpleados[i].id == idem){
+            if (this.listaEmpleados[i].id == ident){
                 this.activa = true;
                 this.empactivo = this.listaEmpleados[i].nombre;
-                console.log("El empleado: " + this.empactivo + " se ha registrado ");
+                console.log("El empleado " + this.empactivo + " se ha registrado ");
             }
         }
+    }
+    
+    agregaArticulo(id, cant){
+        var prodPrecio = 0; 
+        var artPrecTot = 0;
+        
+        for (var i=0; i<this.listaProductos.length; i++){
+            if (this.listaProductos[i].idproducto == id){
+                prodPrecio = this.listaProductos[i].precio;
+                console.log ("\n " + this.listaProductos[i].articulo + " se a agregado con: " + cant + " Unidades");
+            }
+        }
+        
+        artPrecTot = cant * prodPrecio;
+        this.total = artPrecTot + this.total;
     }
     
     iniciarCuenta(){
         if (this.empactivo){
             this.cuentaActiva = true;
+            console.log("\n Cuenta iniciada");
         }
         
         if (this.cuentaActiva){
@@ -85,17 +101,57 @@ class CajaRegistradora {
     }
     
     finalizarCuenta(){
-        
+        this.cuentaActiva = false;
+        console.log("\n La cuenta es de: " + this.total + "€");
+        this.total = 0;
     }
 }
 
-micaja = new CajaRegistradora();
-micaja.introducirEmpleado(1, "Gustavo", "granada", 10);
-micaja.introducirEmpleado(2, "Juan", "granada", 5);
-micaja.introducirEmpleado(3, "Diana", "granada", 15);
+function cargaEmpleados (){
 
-micaja.identificarEmpleado(2, "Juan");
+    micaja.introducirEmpleado(1, "Gustavo", "granada", 10);
+    micaja.introducirEmpleado(2, "Juan", "granada", 5);
+    micaja.introducirEmpleado(3, "Diana", "granada", 15);
 
-micaja.introducirProducto("pañuelos", 0.94);
-micaja.introducirProducto("pipas", 1.10);
-micaja.introducirProducto("barra de pan", 0.45);
+}
+
+function cargaProductos (){
+    
+    micaja.introducirProducto("1","pañuelos", 0.94);
+    micaja.introducirProducto("2","pipas", 1.10);
+    micaja.introducirProducto("3","barra de pan", 0.45);
+    
+}
+
+function iniciaCaja(){
+    
+    micaja = new CajaRegistradora();
+    cargaEmpleados();
+    
+    if(micaja.activa == false){
+        micaja.identificarEmpleado(prompt("\n Indique su ID de Empleado: "));
+    }
+    
+    cargaProductos();
+    
+    do {
+        iniciar = prompt("\n ¿Desea iniciar la venta? si/no");
+        if (iniciar == "si"){
+            micaja.iniciarCuenta();
+        }
+    }while(micaja.cuentaActiva == false);
+    
+    do{
+        
+        var idArt = prompt("\n Indique id del artículo: ");
+        
+        if (idArt == "fincuenta"){
+            micaja.finalizarCuenta();
+            break;
+        }
+        var cant = parseInt(prompt("\n Indique cantidad: "));
+        
+        micaja.agregaArticulo(idArt, cant);
+        
+    }while(micaja.cuentaActiva);
+}
